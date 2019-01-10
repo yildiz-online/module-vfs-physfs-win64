9fee26c968c0472912bb1c61d934cb3072b8c2b1
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-01-10 21:58:35 +0100

    [YE-0] Fix path.

--- src/main/scripts/win64/build.sh
+++ src/main/scripts/win64/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cmake . \
--DPHYSFS_LIBRARY="physicsfs/win64/lib/libphysfs.dll.a"
+-DPHYSFS_LIBRARY="physicsfs/win64/lib/libphysfs.a"
 -DPHYSFS_INCLUDE_DIR="physicsfs/win64/include" \
 -DLIBRARY_OUTPUT_PATH="../../../../target/classes/win64" \
 -DCMAKE_TOOLCHAIN_FILE=mingw-toolchain.cmake
