dfec8d16819bf790676f7330582737cecb79d070
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-01-11 11:43:00 +0100

    [YE-0] Fix build.

--- src/main/scripts/win64/build.sh
+++ src/main/scripts/win64/build.sh
@@ -1,21 +1,28 @@
 #!/usr/bin/env bash
 
 cmake . \
--DPHYSFS_LIBRARY="physicsfs/win64/lib/libphysfs.a" \
+-DCMAKE_MODULE_PATH=. \
+-DCMAKE_BUILD_TYPE=Release \
 -DPHYSFS_INCLUDE_DIR="physicsfs/win64/include" \
+-DPHYSFS_LIBRARY="physicsfs/win64/lib/libphysfs.dll" \
 -DLIBRARY_OUTPUT_PATH="../../../../target/classes/win64" \
 -DCMAKE_TOOLCHAIN_FILE=mingw-toolchain.cmake
 
 make
 r1=$?
 
+cp physicsfs/win64/bin/libphysfs.dll ../../../../target/classes/win64/libphysfs.dll
+cp libstdc++-6.dll ../../../../target/classes/win64/libstdc++-6.dll
+cp libgcc_s_seh-1.dll ../../../../target/classes/win64/libgcc_s_seh-1.dll
+
 rm -R CMakeFiles
 rm CMakeCache.txt
 rm cmake_install.cmake
 rm Makefile
 rm -r physicsfs
-rm -r ../../c++
-rm -r ../../includes
-rm -r ../win64
+rm -r java
+rm libgcc_s_seh-1.dll
+rm libstdc++-6.dll
+rm mingw-toolchain.cmake
 
 return $r1
