99c30403749496a09481a4fe881d07346021160f
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-01-12 18:56:52 +0100

    [YE-0] Support build from windows.

---
+++ src/main/scripts/win64/build.bat
@@ -0,0 +1,19 @@
+cmake . ^
+-DCMAKE_MODULE_PATH=. ^
+-DCMAKE_BUILD_TYPE=Release ^
+-DPHYSFS_INCLUDE_DIR="physicsfs/win64/include" ^
+-DPHYSFS_LIBRARY="physicsfs/win64/lib/libphysfs.dll" ^
+-DLIBRARY_OUTPUT_PATH="../../../../target/classes/win64" ^
+-G "MinGW Makefiles"
+
+SET r1=
+mingw32-make
+
+cd ..
+copy physicsfs/win64/bin/libphysfs.dll ../../../../target/classes/win64
+copy libstdc++-6.dll ../../../../target/classes/win64
+copy libgcc_s_seh-1.dll ../../../../target/classes/win64
+
+EXIT /B $r1
+
+
