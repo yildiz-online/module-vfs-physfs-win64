4e5210d4f6e69685863dfb0adbe5c8a98613f2b9
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-01-13 23:16:58 +0100

    [YE-0] Static link.

--- src/main/scripts/win64/build.bat
+++ src/main/scripts/win64/build.bat
@@ -2,7 +2,7 @@ cmake . ^
 -DCMAKE_MODULE_PATH=. ^
 -DCMAKE_BUILD_TYPE=Release ^
 -DPHYSFS_INCLUDE_DIR="physicsfs/win64/include" ^
--DPHYSFS_LIBRARY="physicsfs/win64/lib/libphysfs.dll.a" ^
+-DPHYSFS_LIBRARY="physicsfs/win64/lib/libphysfs.a" ^
 -DLIBRARY_OUTPUT_PATH="../../../../target/classes/win64" ^
 -G "MinGW Makefiles"
 
@@ -10,7 +10,6 @@ SET r1=
 mingw32-make
 
 cd ..
-copy physicsfs/win64/bin/libphysfs.dll ../../../../target/classes/win64
 copy libstdc++-6.dll ../../../../target/classes/win64
 copy libgcc_s_seh-1.dll ../../../../target/classes/win64
 
--- src/main/scripts/win64/build.sh
+++ src/main/scripts/win64/build.sh
@@ -3,14 +3,13 @@ cmake . \
 -DCMAKE_MODULE_PATH=. \
 -DCMAKE_BUILD_TYPE=Release \
 -DPHYSFS_INCLUDE_DIR="physicsfs/win64/include" \
--DPHYSFS_LIBRARY="physicsfs/win64/bin/libphysfs.dll" \
+-DPHYSFS_LIBRARY="physicsfs/win64/lib/libphysfs.a" \
 -DLIBRARY_OUTPUT_PATH="../../../../target/classes/win64" \
 -DCMAKE_TOOLCHAIN_FILE=mingw-toolchain.cmake
 
 make
 r1=$?
 
-cp physicsfs/win64/bin/libphysfs.dll ../../../../target/classes/win64/libphysfs.dll
 cp libstdc++-6.dll ../../../../target/classes/win64/libstdc++-6.dll
 cp libgcc_s_seh-1.dll ../../../../target/classes/win64/libgcc_s_seh-1.dll
 
