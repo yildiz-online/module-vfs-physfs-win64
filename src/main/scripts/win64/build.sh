79a0864a36e8bf44f00f517fecbc15b83edd9a76
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-05-01 10:00:38 +0200

    [FEATURE] Support build number

--- src/main/scripts/win64/build.sh
+++ src/main/scripts/win64/build.sh
@@ -4,11 +4,8 @@ source ../../../../target/classes/project.txt
 
 VERSION=${VERSION%"-SNAPSHOT"}
 
-echo "Build number"
-echo $BUILD_NUMBER
-
 echo "1 VERSIONINFO" > version.rc
-echo "FILEVERSION $VERSION,0" | tr . , >> version.rc
+echo "FILEVERSION $VERSION,$BUILD_NUMBER" | tr . , >> version.rc
 echo "PRODUCTVERSION $VERSION,0" | tr . , >> version.rc
 echo "FILEFLAGSMASK 0x17L" >> version.rc
 echo "FILEFLAGS 0x0L" >> version.rc
@@ -21,7 +18,7 @@ echo "    BEGIN" >> version.rc
 echo "        BLOCK \"040904b0\"" >> version.rc
 echo "        BEGIN" >> version.rc
 echo "            VALUE \"FileDescription\", \"Yildiz-Engine VFS with PhysFS implementation.\"" >> version.rc
-echo "            VALUE \"FileVersion\", \"$VERSION,0\"" | tr . , >> version.rc
+echo "            VALUE \"FileVersion\", \"$VERSION,$BUILD_NUMBER\"" | tr . , >> version.rc
 echo "            VALUE \"InternalName\", \"libyildizphysfs.dll\\\0\"" >> version.rc
 echo "            VALUE \"LegalCopyright\", \"Copyright (c) 2019 Gregory Van den Borre\\\0\"" >> version.rc
 echo "            VALUE \"OriginalFilename\", \"libyildizphysfs.dll\\\0\"" >> version.rc
