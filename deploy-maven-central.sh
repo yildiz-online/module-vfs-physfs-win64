e8ea585f534670cf8b2798cd58b04a5e73f6f22e
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-05-15 08:03:51 +0200

    [WIP] cmake not found.

--- deploy-maven-central.sh
+++ deploy-maven-central.sh
@@ -21,6 +21,10 @@ SONAR_ORGANIZATION=$(echo ${SECRETS} | jq -r '.data.SONAR_ORGANIZATION')
 
 echo "Building $BRANCH branch"
 
+echo ">>>>>>>>>"
+echo ${PATH}
+echo "<<<<<<<<<"
+
 if [[ "$BRANCH" = "develop" ]]; then
   openssl version -a
   openssl aes-256-cbc -pass pass:$OPENSSL_PWD -in ../build-resources/private-key.gpg.enc -out private-key.gpg -d && gpg --import --batch private-key.gpg && mvn -V -s ../build-resources/settings.xml org.jacoco:jacoco-maven-plugin:prepare-agent clean deploy -Pbuild sonar:sonar -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=$SONAR_ORGANIZATION -Dsonar.login=$SONAR
