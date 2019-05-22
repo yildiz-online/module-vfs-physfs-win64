79a855ea8f6f826351b3b78c5ec6b29236f9147d
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-05-22 17:08:22 +0200

    Update deploy-maven-central.sh

--- deploy-maven-central.sh
+++ deploy-maven-central.sh
@@ -19,7 +19,7 @@ echo "Building $BRANCH branch"
 
 if [ "$BRANCH" = "develop" ]; then
   openssl version -a
-  openssl aes-256-cbc -pass pass:$OPENSSL_PWD -in ../build-resources/private-key.gpg.enc -out private-key.gpg -d && gpg --import --batch private-key.gpg && mvn -X -V -s ../build-resources/settings.xml clean deploy
+  openssl aes-256-cbc -pass pass:$OPENSSL_PWD -in ../build-resources/private-key.gpg.enc -out private-key.gpg -d && gpg --import --batch private-key.gpg && mvn -V -s ../build-resources/settings.xml clean deploy
 elif [ "$BRANCH" = "master" ]; then
   openssl version -a
   openssl aes-256-cbc -pass pass:$OPENSSL_PWD -in ../build-resources/private-key.gpg.enc -out private-key.gpg -d && gpg --import --batch private-key.gpg && mvn -V -s ../build-resources/settings.xml clean deploy
