862452d33e2ed90e521fd2f9fd52cfb89b2ccfa6
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-05-23 14:06:11 +0200

    Update deploy-maven-central.sh

--- deploy-maven-central.sh
+++ deploy-maven-central.sh
@@ -19,11 +19,11 @@ echo "Building $BRANCH branch"
 
 if [ "$BRANCH" = "develop" ]; then
   openssl version -a
-  openssl aes-256-cbc -pass pass:$OPENSSL_PWD -in ../build-resources/private-key.gpg.enc -out private-key.gpg -d && gpg --import --batch private-key.gpg && mvn -V -s ../build-resources/settings.xml clean deploy
+  openssl aes-256-cbc -pass pass:$OPENSSL_PWD -in ../build-resources/private-key.gpg.enc -out private-key.gpg -d && gpg --import --batch private-key.gpg && mvn -V -s ../build-resources/settings.xml clean deploy -Pbuild
 elif [ "$BRANCH" = "master" ]; then
   openssl version -a
-  openssl aes-256-cbc -pass pass:$OPENSSL_PWD -in ../build-resources/private-key.gpg.enc -out private-key.gpg -d && gpg --import --batch private-key.gpg && mvn -V -s ../build-resources/settings.xml clean deploy
-  mvn -V -s ../build-resources/settings.xml deploy -Dmaven.plugin.nexus.skip
+  openssl aes-256-cbc -pass pass:$OPENSSL_PWD -in ../build-resources/private-key.gpg.enc -out private-key.gpg -d && gpg --import --batch private-key.gpg && mvn -V -s ../build-resources/settings.xml clean deploy -Pbuild
+  mvn -V -s ../build-resources/settings.xml deploy -Dmaven.plugin.nexus.skip -Pbuild
 else
-  mvn -V -s ../build-resources/settings.xml clean package
+  mvn -V -s ../build-resources/settings.xml clean package -Pbuild
 fi
